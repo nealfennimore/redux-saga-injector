5453bf492029bc19ab769d8ace40132a57e44721
Neal Fennimore <hi@neal.codes>
2018-06-03 21:50:58 -0700

    Revert to old flow to see if fixes issue

--- src/sagas/queue.js
+++ src/sagas/queue.js
@@ -1,7 +1,7 @@
-import { call, take, put, spawn, fork, cancel, race, takeEvery } from 'redux-saga/effects';
+import { call, take, put, fork, spawn, cancel, takeEvery } from 'redux-saga/effects';
+import { delay } from 'redux-saga';
 import * as actions from 'src/actions/sagas';
 import { runSagas } from 'src/sagas/run';
-import { timeout } from 'src/sagas/utils';
 import defaultOptions from './options';
 
 /**
@@ -36,7 +36,9 @@ export function removeFromQueue( queue, action ) {
 export function* queueSagaRunner( queue, action ) {
     if( action.sagas.length ) {
         yield call( addToQueue, queue, action );
-        yield spawn( runSagas, action );
+        // This needs to be spawn in order to
+        // prevent hanging with an attached 'fork'
+        return yield spawn( runSagas, action );
     }
 }
 
@@ -65,28 +67,6 @@ export function createQueue() {
 }
 
 /**
- * Starts up queue
- * @description loops until no more RUN_SAGAS are received
- * @export
- * @param {Set} queue Queue
- * @param {{preloadTimeout: number}} options Options
- * @param {number} [options.preloadTimeout] Timeout duration
- */
-export function* startQueue( queue, options ) {
-    while ( true ) {
-        const { runAction } = yield race( {
-            runAction: take( actions.RUN_SAGAS ),
-            timedOut: call( timeout, options.preloadTimeout ),
-        } );
-        if( runAction ) {
-            yield fork( queueSagaRunner, queue, runAction );
-        } else {
-            break; // Finish
-        }
-    }
-}
-
-/**
  * Saga for server side rendering
  * Creates a queue, runs sagas, and completes when queue is empty
  *
@@ -97,11 +77,17 @@ export function* startQueue( queue, options ) {
 export function* preloadQueue( options = defaultOptions ) {
     const queue = yield call( createQueue );
 
+    // Start watching for RUN_SAGAS actions
+    const runTask = yield takeEvery( actions.RUN_SAGAS, queueSagaRunner, queue );
+
     // Start watching for finished or cancel actions
     const emptyTask = yield takeEvery( [actions.SAGAS_FINISHED, actions.CANCEL_SAGAS], queueEmptier, queue );
 
-    // Start queue and proceed after receiving all RUN_SAGAS actions
-    yield call( startQueue, queue, options );
+    // Give a delay for preloading
+    yield call( delay, options.preloadTimeout );
+
+    // Receive no more sagas
+    yield cancel( runTask );
 
     // If we have still have items in the queue, then wait til all sagas finish or are cancelled
     if( queue.size ) {
