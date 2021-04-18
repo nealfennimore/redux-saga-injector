6fcd8cdc63ba4f98460f88d8a015a81bde2535e7
Neal Fennimore <hi@neal.codes>
2021-04-17 20:50:29 -0400

    Remove unused

--- src/hooks/__tests__/useSagaInjector.spec.js
+++ src/hooks/__tests__/useSagaInjector.spec.js
@@ -1,7 +1,4 @@
-import React, {
-    Component
-} from 'react';
-import { useDispatch } from 'react-redux';
+import React from 'react';
 import renderer from 'react-test-renderer';
 import useSagaInjector from '../useSagaInjector';
 
